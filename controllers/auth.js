69fc98b37dfea886b9015b829130ec909ae923e0
Umesh <umeshmane575@gmail.com>
2023-08-28 14:45:48 +0530

    Server is working fine

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ exports.createOrUpdateUser = async (req, res) => {
 
   const user = await User.findOneAndUpdate(
     { email },
-    { name, picture },
+    { name: email.split("@")[0], picture },
     { new: true }
   );
   if (user) {
@@ -14,10 +14,17 @@ exports.createOrUpdateUser = async (req, res) => {
   } else {
     const newUser = await new User({
       email,
-      name,
+      name: email.split("@")[0],
       picture,
     }).save();
     console.log("USER CREATED", newUser);
     res.json(newUser);
   }
+};
+
+exports.currentUser = async (req, res) => {
+  User.findOne({ email: req.user.email }).exec((err, user) => {
+    if (err) throw new Error(err);
+    res.json(user);
+  });
 };

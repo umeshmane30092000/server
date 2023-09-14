d6b09ad4bfa673fcf6a781dfc31ba60ebcd12c76
Umesh <umeshmane575@gmail.com>
2023-09-14 10:44:02 +0530

    i have commit out that enum array of color and brand so that in the frontend we can have no color option all backend workig fine

--- models/product.js
+++ models/product.js
@@ -52,11 +52,11 @@ const productSchema = new mongoose.Schema(
     },
     color: {
       type: String,
-      enum: ["Black", "Brown", "Silver", "White", "Blue"],
+      // enum: ["Black", "Brown", "Silver", "White", "Blue"],
     },
     brand: {
       type: String,
-      enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
+      // enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
     },
     // ratings: [
     //   {

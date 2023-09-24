aefb9faaaacdc2004acedd9df8864023afc68083
Umesh <umeshmane575@gmail.com>
2023-09-24 15:56:31 +0530

    we have written routes get and getrelated method in the controlles and from fronend we have made request to backend to get product we have use that functins in the frontend | basically from frontend req come to backend to that routes and contrllles method req database featch related fromduct and it will show on the home page so that frontend->backend->database work syncronously

--- routes/product.js
+++ routes/product.js
@@ -14,6 +14,7 @@ const {
   list,
   productsCount,
   productStar,
+  listRelated,
 } = require("../controllers/product");
 
 // routes
@@ -28,5 +29,7 @@ router.put("/product/:slug", authCheck, adminCheck, update);
 router.post("/products", list);
 // rating
 router.put("/product/star/:productId", authCheck, productStar);
+// related
+router.get("/product/related/:productId", listRelated);
 
 module.exports = router;
